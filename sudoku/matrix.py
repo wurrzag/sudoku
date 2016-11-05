727a0844c49f3e852376fb8ac056a424b3de28aa
Josef <pjz2@centrum.cz>
2016-11-05 12:29:42 +0100

    class-oriented & explicit-respecting

---
+++ sudoku/matrix.py
@@ -0,0 +1,102 @@
+class Zweiboard:
+  def __init__(self, matrix = [], shadow = []):
+    self.board = [[0] * 9 for _ in range(9)] # array of integers as a board
+    self.shadow = [['123456789'] * 9 for _ in range(9)] # array of strings as a list of candidates for every field in board
+    if len(matrix): self.__load(matrix, shadow)
+  
+  def load(self, board, shadow = []):
+    for i in range(81): self.board[i//9][i%9] = board[i//9][i%9]
+    if len(shadow):
+      for i in range(81): self.shadow[i//9][i%9] = shadow[i//9][i%9]
+    
+  def row(self, row): return self.board[row]
+  def column(self, col): return [row[col] for row in self.board]
+  def area(self, area):
+    out = []
+    for i in range(9):
+      x = 3* (area//3) + i//3; y = 3* (area%3) + i%3
+      out += [self.board[x][y]]
+    return out
+  
+  
+  # *** showing the board and/or shadow board
+  def show(self, mask = [1, 0]): # default: show the board and skip the shadow board
+    if mask[0]: # showing the board
+      for row in self.board: print(8* ' ' + str(row).replace(',', '').replace('[', '').replace(']', '').replace('0', chr(183)))
+      print(36* '_' + ' sum of candidates = ' + str(self.count_shadows()))
+    if mask[1]: # showing shadow board
+      for row in self.shadow:
+        line = ''
+        for cell in row: line += '%8s' % cell
+        print(line)
+        
+  # *** sum of candidates (shadows) for all slots
+  def count_shadows(self, trigger = True): # by default, count shadows. by alternative, count just number of fields with nonzero shadows
+    count = 0
+    for row in self.shadow:
+      for string in row: count += len(string) if trigger else 1 if len(string) else 0
+    return count
+
+  # *** sum of yet unfilled slots
+  def count_unknowns(self):
+    count = 0
+    for row in self.board:
+      for slot in row:
+        if not slot: count += 1
+    return count
+
+      
+  # *** writes specific number into slot (and shadow to zero) and
+  # *** removes number's candidates from row, column and area it belongs to
+  def write(self, row, col, what):
+    out = self.__class__(self.board, self.shadow)
+    out.board[row][col] = what; out.shadow[row][col] = ''
+    out = out.lighten('column', row, col, str(what))
+    out = out.lighten('row'   , row, col, str(what))
+    out = out.lighten('area'  , row, col, str(what))
+    return out
+
+  # *** removes given type of candidates (1-9) in given scale (row,column,area)
+  def lighten(self, typ, row, col, mask, exceptions = []):
+    out = self.__class__(self.board, self.shadow)
+    for what in mask:
+      if typ == 'slot':
+        if not [row, col] in exceptions: out.shadow[row][col] = out.shadow[row][col].replace(what, '')
+      if typ == 'row':
+        for y in range(9):
+          if not [row, y] in exceptions: out.shadow[row][y] = out.shadow[row][y].replace(what, '')
+      if typ == 'column':
+        for x in range(9):
+          if not [x, col] in exceptions: out.shadow[x][col] = out.shadow[x][col].replace(what, '')
+      if typ == 'area':
+        for slot in range(9):
+          x = 3* (row //3) + slot //3; y = 3* (col //3) + slot %3
+          if not [x, y] in exceptions: out.shadow[x][y] = out.shadow[x][y].replace(what, '')
+    return out    
+  
+  # *** writes all sure-candidates (fields with only one shadow) on board (note: methods.hidden_elements(1) solves the rest of them)
+  def write_singles(self):
+    export = self.__class__(self.board, self.shadow); again = True
+    while(again):
+      again = False         # if there is nothing to add to desk, end this loop
+      for row in range(9):
+        for col in range(9):
+          if len(export.shadow[row][col]) == 1: # only one candidate for cell means sure-candidate
+            again = True    # if we are filling a cell in sudoku, there is a chance another run will reval new one
+            export = export.write(row, col, int(export.shadow[row][col])) # write the sure-candidate to board
+    return export
+
+  __load = load
+  
+  
+class Stack_of_boards: # class necessary for proper work of paths.try_branches()
+  def __init__(self): self.stack = []
+  def add(self, board): self.stack += [board]
+  def ret(self): return self.stack
+  def length(self): return len(self.stack)
+  def get(self):
+    if self.length():
+      last = self.stack[-1]; self.stack = self.stack[:-1]
+      return last
+    return False
+
---
+++ sudoku/sudoku.py
@@ -0,0 +1,58 @@
+# TENHLE SOUBOR MA BYT O ADRESAR VYS
+# NAHRAL A ADDNUL JSEM JEJ SEM, PROTOZE GIT COMMIT MI NEPISE SUDOKU.PY JAKO NOVOU ZMENU, TAK PROSTE ZALOHA
+
+from sudoku import basic # basic functions
+from sudoku import data  # some sudoku's from solve book and internet
+from sudoku import paths # analysis definition and brute-force method
+
+SW_MASK = [1, 0] # whether to show board and shadow board in show(). default is [1, 0]. to see the result of any one method, switch it to [1, 1]. no report is [0, 0]
+SW_BASIC_ANALYSIS = 0b11000  # five bytes to switch five methods on/off as a part of basic analysis. default is 0b11000 and 0b10000 is still reasonable (but tested as overally slower even when locally faster)
+SW_FULL_ANALYSIS  = 0b11111  # five bytes to switch five methods on/off as a part of  full analysis. default is 0b11111 and 0b11110 is still reasonable
+                             # methods are: NAKED_PAIRS, HIDDEN_COUPLES, NAKED_TRIPLES, HIDDEN_TRIPLES, INTERSECTION_REMOVAL
+
+def solve(problem):
+  # *** printing the final result(s) method
+  def last_show(typ, data):
+    if typ == 'normal': last_show_two(data)
+    elif typ == 'special' and len(data) > 1:
+      print(36* ' ' + 'MULTIPLE SOLUTIONS:')
+      for i in range(len(data)): print(' --- solution no. ' + str(i+1)); last_show_two(data.get())
+  def last_show_two(board):
+    board.show(SW_MASK); print(bs.validity_test(board))
+    
+  # *** the real start od program
+  bs = basic.Manager()
+  gate = paths.Manager(bs, SW_BASIC_ANALYSIS, SW_FULL_ANALYSIS) # instance of algorithms classes
+  sudoku = gate.meth.new_board(problem) # load the board
+  sudoku = gate.meth.initial_reduce(sudoku) # adjust the shadow mask properly
+                                        # other methods from methods.py (naked pairs etc) can be called in this manner
+  sudoku.show(SW_MASK)                  # show the task at hand
+  sudoku = sudoku.write_singles()       # make the most primitive attempt to reduce the candidates
+  sudoku = gate.full_analysis(sudoku)   # proper attempt to reduce candidates and solve the board
+
+  validity = bs.validity_test(sudoku); special_end = False # finds out the state of board
+  if validity == 'unfinished board':    # if there is some job left
+    result = gate.try_branches(sudoku)  # run the brute force method
+    if   result.length() == 1: sudoku = result.get() # if there is just one matrix as a possible solution, load it into the original board
+    elif result.length() == 0: print("Something is wrong (Sheldon's personal seat is Penny-butted)")
+    else: special_end = True            # triggering the 'alternative solutions' event
+  if special_end:                       # alternative returning value
+    last_show('special', result); return result
+  else:                                 # casual returning value
+    last_show('normal', sudoku); return sudoku
+# *** the end
+
+solve(data.problem)
+
+
+'''
+# for one in data.get_all_valid_sudokus(): solve(one) # this way i found the bug which is:
+# BUG BUG BUG BUG BUG BUG BUG:
+switch = True # run the program and then change this switch and run it again to see the bug 
+if switch:
+  solve(data.problem); solve(data.problem2) # works right
+else:
+  solve(data.problem2); solve(data.problem) # doesn't solve the second one
+  
+# aditional info: in case of malfunction, program runs the try_branches() but it just don't find the solution. Funny message 16 lines up from here serves as a proof of that
+'''
